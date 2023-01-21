4026bcba668aeac8eab542860caa0d1d09ebfbcd
Kannan R <kannanr60@gmail.com>
2023-01-21 22:17:02 +0530

    Variables and Strings examples

---
+++ src/main/kotlin/VariablesandStrings.kt
@@ -0,0 +1,15 @@
+package src.main.kotlin
+
+fun main() {
+    println("Hi this is \\ a backslash") //Print backslash
+    val catName = "My Cat's name is \"Fluffy\""
+    println(catName) //Print Double Quotes String
+    val slashes = "Two types of Slashes: \\ and /"
+    println(slashes) //Print two backslash & forward slash
+    val websiteName = "https://www.google.com"
+    println(websiteName) //Print Web URL
+
+    //Draw this Bunny
+    val bunnyDraw = "(\\(\\\n(-.-)\nO_(\")(\")"
+    println(bunnyDraw) //Print sleeping bunny pic
+}
